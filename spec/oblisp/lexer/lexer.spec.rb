55813c60ff1c9669195d9c43f0f4099144f11508
Oblivious <thanos.papap42@gmail.com>
2022-06-17 04:24:01 +0300

    Implemented most lex tokenisation
    TODO -> Still have to implement alternate base literals.

--- spec/oblisp/lexer/lexer.spec.rb
+++ spec/oblisp/lexer/lexer.spec.rb
@@ -1,43 +1,193 @@
 describe Lexer do
     it "contains the input text, lexer position and current character" do
-        l = Lexer.new("(42 factorial)");
-        expect(l.text).to eq "(42 factorial)";
+        l = Lexer.new "file.obl", "(42 factorial.)".chars;
+        expect(l.text).to eq "(42 factorial.)".chars;
         expect(l.pos).to eq -1;
     end
 
-    it "#advance" do
-        l = Lexer.new("(42 factorial)");
+    context "#next_character" do
+        it "properly advances characters in the array" do
+            l = Lexer.new "file.obl", "(42 factorial.)".chars;
 
-        current_char = l.advance;
-        expect(current_char.c).to eq "(";
-        expect(l.pos).to eq 0;
+            current_char = l.next_character;
+            expect(current_char).to eq "(";
+            expect(l.pos).to eq 0;
 
-        current_char = l.advance
-        expect(current_char.c).to eq "4";
-        expect(l.pos).to eq 1;
+            current_char = l.next_character;
+            expect(current_char).to eq "4";
+            expect(l.pos).to eq 1;
+
+            current_char = l.next_character;
+            current_char = l.next_character;
+            current_char = l.next_character;
+            expect(current_char).to eq "f";
+            expect(l.pos).to eq 4;
+        end
+
+        it "reads all characters until the list ends" do
+            l = Lexer.new "file.obl", ["1","2","3","4","5"];
+    
+            c1 = l.next_character;
+            c2 = l.next_character;
+            c3 = l.next_character;
+            c4 = l.next_character;
+            c5 = l.next_character;
+            expect(c1).to eq "1";
+            expect(c2).to eq "2";
+            expect(c3).to eq "3";
+            expect(c4).to eq "4";
+            expect(c5).to eq "5";
+
+            # String reaches EOF
+            c6 = l.next_character;
+            expect(c6).to eq nil;
+        end
     end
 
-    it "reads all tokens until the string ends" do
-        l = Lexer.new("12345");
+    context "#prev_character" do
+        it "goes back to a previous character in the list" do
+            l = Lexer.new "file.obl", "(42 factorial.)".chars;
 
-        curr = l.advance;
-        expect(curr.c).to eq "1";
-        curr = l.advance;
-        expect(curr.c).to eq "2";
-        curr = l.advance;
-        expect(curr.c).to eq "3";
-        curr = l.advance;
-        expect(curr.c).to eq "4";
-        curr = l.advance;
-        expect(curr.c).to eq "5";
+            c = l.next_character;
+            c = l.next_character;
+            c = l.next_character;
+            c = l.prev_character;
 
-        # String reaches EOF
-        curr = l.advance;
-        expect(curr.c).to eq nil;
+            expect(c).to eq "4";
+        end
     end
 
-    it "creates a list of tokens" do
-        l = Lexer.new("(42 factorial)");
-        expect(l.make_tokens).to eq [];
+    context "string extensions" do
+        it "matches regular expressions" do
+            expect(" ".matches(RegexMatchers::WHITESPACE)).to be true;
+            expect("\n".matches(RegexMatchers::NEWLINE)).to be true;
+            expect("#".matches(RegexMatchers::COMMENT)).to be true;
+            expect("9".matches(RegexMatchers::NUMBER)).to be true;
+            expect("k".matches(RegexMatchers::LETTER)).to be true;
+            expect("?".matches(RegexMatchers::SYMBOL)).to be true;
+            expect("'".matches(RegexMatchers::QUOTE)).to be true;
+            expect("self".matches(RegexMatchers::KEYWORDS)).to be true;
+            expect("super".matches(RegexMatchers::KEYWORDS)).to be true;
+        end
+    end
+
+    it "prints error messages" do
+        l = Lexer.new "file.obl", "(42 factorial.)".chars;
+        response = l.error("random exception");
+        expect(response).to eq [];
+    end
+
+    context "#make_tokens" do
+        it "correctly counts the number of lines" do
+            l = Lexer.new "file.obl", "(42 factorial.\nx = 2 + 3\ny = 5)".chars;
+            l.make_tokens;
+
+            expect(l.lineno).to eq 3;
+        end
+
+        it "ignores comment lines" do
+            l = Lexer.new "file.obl", "(1 2 3\n#Comment Stuff#\n5)".chars;
+            tokens = l.make_tokens;
+            expect(tokens.size).to eq 6;
+        end
+
+        it "tokenizes symbols" do
+            l = Lexer.new "file.obl", "(!@$%^&*)".chars;
+            tokens = l.make_tokens;
+            expect(tokens.size).to eq 9;
+            expect(tokens[3].value).to eq "$";
+        end
+
+        it "tokenizes integers" do
+            l = Lexer.new "file.obl", "(42 msg.)".chars;
+            tokens = l.make_tokens;
+
+            expect(tokens[1].value).to eq "42";
+            expect(tokens[1].type).to eq Type::INTEGER;
+            expect(tokens[1].line_number).to eq 1;
+        end
+
+        it "draws an error when trying to tokenize an integer starting with 0" do
+            l = Lexer.new "file.obl", "(042 msg.)".chars;
+            tokens = l.make_tokens;
+        end
+
+        # xit "tokenizes big integers" do
+        # end
+
+        it "tokenizes floats" do
+            l = Lexer.new "file.obl", "(42.7 msg.)".chars;
+            tokens = l.make_tokens;
+            expect(tokens[1].value).to eq "42.7";
+            expect(tokens[1].type).to eq Type::FLOAT;
+            expect(tokens[1].line_number).to eq 1;
+
+            l = Lexer.new "file.obl", "(0.7 msg.)".chars;
+            tokens = l.make_tokens;
+            expect(tokens[1].value).to eq "0.7";
+            expect(tokens[1].type).to eq Type::FLOAT;
+            expect(tokens[1].line_number).to eq 1;
+
+            l = Lexer.new "file.obl", "(.7 msg.)".chars;
+            tokens = l.make_tokens;
+            expect(tokens[1].value).to eq ".";
+            expect(tokens[2].value).to eq "7";
+            expect(tokens[3].value).to eq "msg";
+            expect(tokens[4].value).to eq ".";
+
+            l = Lexer.new "file.obl", "(124.0 msg.)".chars;
+            tokens = l.make_tokens;
+            expect(tokens[1].value).to eq "124.0";
+            expect(tokens[2].value).to eq "msg";
+            expect(tokens[3].value).to eq ".";
+        end
+
+        # xit "tokenizes big floats" do
+        # end
+
+        # xit "tokenizes binary literals" do
+            # TODO Works with 'b' and 'B'
+        # end
+
+        # xit "tokenizes hexadecimal literals" do
+        # end
+
+        # xit "tokenizes octal literals" do
+        # end
+
+        # xit "tokenizes string literals" do
+        # end
+
+        it "tokenizes identifiers" do
+            l = Lexer.new "file.obl", "(42 factorial.\nx = (2 times: 3)\n3 plus: 4)".chars;
+            tokens = l.make_tokens;
+            expect(tokens.size).to eq 17;
+            expect(tokens[2].value).to eq "factorial";
+            expect(tokens[4].value).to eq "x";
+            expect(tokens[8].value).to eq "times";
+            expect(tokens[13].value).to eq "plus";
+        end
+
+        it "tokenizes the `self` keyword" do
+            l = Lexer.new "file.obl", "(self puts.)";
+            tokens = l.make_tokens;
+            expect(tokens.size).to eq 5;
+            expect(tokens[1].value).to eq "self";
+        end
+
+        it "tokenizes the `super` keyword" do
+            l = Lexer.new "file.obl", "(super puts.)";
+            tokens = l.make_tokens;
+            expect(tokens.size).to eq 5;
+            expect(tokens[1].value).to eq "super";
+        end
+
+        it "prints the token table" do
+            l = Lexer.new "file.obl", "(super puts.)";
+            tokens = l.make_tokens;
+            expect(tokens.size).to eq 5;
+
+            l.print_token_table;
+        end
     end
 end
--- src/oblisp/lexer/lexer.rb
+++ src/oblisp/lexer/lexer.rb
@@ -1,44 +1,159 @@
-class Lexer
-    attr_accessor :text, :pos, :current_char;
-    private attr_writer :pos, :current_char;
+require_relative "./type"
 
-    def initialize(text)
-        @text = text;
-        @pos = -1;
+module RegexMatchers
+    WHITESPACE = [" ","\t","\r","\f"];
+    NEWLINE = ["\n"];
+    COMMENT = ["#"];
+
+    NUMBER = ["0","1","2","3","4","5","6","7","8","9"];
+    LETTER = [
+        "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+        "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"
+    ];
+    SYMBOL = ["+","-","*","/","\\","~","<",">","=","@","%","|","&","?","!","^","_",",","`",";","$","`"];
+    SYNTAX_SYMBOL = ["(",")","[","]","{","}",":","."];
+    QUOTE = ["\"","'"];
+
+    KEYWORDS = ["self","super"];
+end
+
+class String
+    def matches(regex)
+        regex.include? self;
     end
 
-    def advance
+    def red
+        "\033[1;31m#{self}\033[0m";
+    end
+
+    def green
+        "\033[1;32m#{self}\033[0m";
+    end
+end
+
+class Lexer
+    attr_accessor :filename, :text, :pos, :lineno, :token_table;
+
+    def initialize(filename, text)
+        @filename = filename;
+        @text = text;
+        @pos = -1;
+        @lineno = 1;
+    end
+
+    def error(message)
+        puts "#{filename}:#{lineno}: #{'error:'.red} #{message}";
+        [];
+    end
+
+    def next_character
         self.pos += 1;
 
         if pos < text.size
-            Character.new text[pos];
+            text[pos];
         else
-            Character.new nil;
+            nil;
         end
     end
 
+    def prev_character
+        self.pos -= 1;
+        text[pos];
+    end
+
+    def print_token_table
+        print "(";
+
+        token_table.each do |token|
+            puts token;
+        end
+
+        print ")";
+    end
+
     def make_tokens
-        tokens = [];
+        token_table = [];
 
-        # TODO Try refactor condition
-        while ((c = advance) && c.c)
-            case c
+        loop do
+            c = next_character;
+            if c == nil
+                break;
+            elsif c.matches(RegexMatchers::NEWLINE)
+                self.lineno += 1;
+            elsif c.matches(RegexMatchers::WHITESPACE)
+                next;
+            elsif c.matches(RegexMatchers::COMMENT)
+                c = next_character;
+                while not c.matches(RegexMatchers::COMMENT)
+                    c = next_character;
+                    if c.matches(RegexMatchers::NEWLINE)
+                        self.lineno += 1;
+                    elsif c == nil
+                        return self.error("unterminated # comment");
+                    end
+                end
+            elsif c.matches(RegexMatchers::NUMBER) and c != '0'
+                actual_type = Type::INTEGER;
+                final_number = c;
+                c = next_character;
+                while c != nil and c.matches(RegexMatchers::NUMBER)
+                    final_number << c;
+                    c = next_character;
+                end
 
-            when c.is_integer
-            when c.is_big_integer
-            when c.is_float
-            when c.is_big_float
+                if c == '.'
+                    final_number << c;
+                    c = next_character;
+                    while c != nil and c.matches(RegexMatchers::NUMBER)
+                        final_number << c;
+                        c = next_character;
+                    end
+                    actual_type = Type::FLOAT;
+                end
+                c = prev_character;
+                token_table << (Token.new final_number, actual_type, lineno);
+            elsif c == '0'
+                final_float = c;
+                c = next_character;
+                if c != '.'
+                    return self.error("unexpected fraction part after numeric literal");
+                else
+                    final_float << c;
+                    c = next_character;
+                    while c != nil and c.matches(RegexMatchers::NUMBER)
+                        final_float << c;
+                        c = next_character;
+                    end
+                end
+                c = prev_character;
+                token_table << (Token.new final_float, Type::FLOAT, lineno);
 
-            when c.is_binary
-            when c.is_hexadecimal
-            when c.is_octal
+            elsif c.matches(RegexMatchers::SYMBOL)
+                token_table << (Token.new c, Type::SYMBOL, lineno);
+            elsif c.matches(RegexMatchers::SYNTAX_SYMBOL)
+                token_table << (Token.new c, Type::SYNTAX_SYMBOL, lineno);
+            elsif c.matches(RegexMatchers::LETTER)
+                final_identifier = c;
+                c = next_character;
+                while c != nil and (c.matches(RegexMatchers::NUMBER) or c.matches(RegexMatchers::SYMBOL) or c.matches(RegexMatchers::LETTER))
+                    final_identifier << c;
+                    c = next_character;
+                end
+                c = prev_character;
 
-            when c.is_string
-            # when c.is_symbol
-
+                if final_identifier.matches(RegexMatchers::KEYWORDS)
+                    if final_identifier == "self"
+                        token_table << (Token.new final_identifier, Type::SELF, lineno);
+                    else
+                        token_table << (Token.new final_identifier, Type::SUPER, lineno);
+                    end
+                else
+                    token_table << (Token.new final_identifier, Type::IDENTIFIER, lineno);
+                end
             end
         end
 
-        tokens;
+        @token_table = token_table;
+        token_table;
     end
 end
