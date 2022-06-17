a429fa6f3025ce3d99f2392c61d92f152a92bfc5
Oblivious <thanos.papap42@gmail.com>
2022-06-17 04:24:25 +0300

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,14 @@
 require "rspec/autorun";
 
 # All source files
-Dir[Dir.pwd + "/src/oblisp/**/*.rb"].each { |f| require f };
+Dir["#{Dir.pwd}/src/oblisp/**/*.rb"].each { |f| require f };
 
 # All spec files
-Dir[Dir.pwd + "/spec/oblisp/**/*.rb"].each { |f| require f };
+Dir["#{Dir.pwd}/spec/oblisp/**/*.rb"].each { |f| require f };
+
+# require "#{Dir.pwd}/spec/oblisp/lexer/file_loader.spec.rb";
+# require "#{Dir.pwd}/spec/oblisp/lexer/lexer.spec.rb";
+# require "#{Dir.pwd}/spec/oblisp/lexer/token.spec.rb";
+# require "#{Dir.pwd}/spec/oblisp/lexer/type.spec.rb";
+
+# require "#{Dir.pwd}/spec/oblisp/version.spec.rb";
