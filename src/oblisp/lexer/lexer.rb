6d5d8eb593a61ccafd065978ac102d9b43c6909d
Oblivious <thanos.papap42@gmail.com>
2022-07-06 00:17:31 +0300

    Update lexer.rb

--- src/oblisp/lexer/lexer.rb
+++ src/oblisp/lexer/lexer.rb
@@ -2,7 +2,7 @@ require_relative "../tokens/type";
 
 module RegexMatchers
     WHITESPACE = [" ","\t","\f"];
-    NEWLINE = ["\r", "\n"];
+    NEWLINE = ["\r","\n"];
 
     NUMBER = ["0","1","2","3","4","5","6","7","8","9"];
     LETTER = [
