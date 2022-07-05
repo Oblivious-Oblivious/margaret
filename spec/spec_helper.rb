cf5cb45ada4f27f6019451036a661c41db15a777
Oblivious <thanos.papap42@gmail.com>
2022-07-05 20:47:28 +0300

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,14 +3,16 @@ require "rspec/autorun";
 # All source files
 Dir["#{Dir.pwd}/src/oblisp/**/*.rb"].each { |f| require f };
 
-# All spec files
-Dir["#{Dir.pwd}/spec/oblisp/**/*.rb"].each { |f| require f };
+# lexer
+require "#{Dir.pwd}/spec/oblisp/lexer/file_loader.spec.rb";
+require "#{Dir.pwd}/spec/oblisp/lexer/lexer.spec.rb";
 
-# require "#{Dir.pwd}/spec/oblisp/lexer/file_loader.spec.rb";
-# require "#{Dir.pwd}/spec/oblisp/lexer/lexer.spec.rb";
-# require "#{Dir.pwd}/spec/oblisp/lexer/token.spec.rb";
-# require "#{Dir.pwd}/spec/oblisp/lexer/type.spec.rb";
+# tokens
+require "#{Dir.pwd}/spec/oblisp/lexer/token.spec.rb";
+require "#{Dir.pwd}/spec/oblisp/lexer/type.spec.rb";
 
-# require "#{Dir.pwd}/spec/oblisp/parser/parser.spec.rb"
+# parser
+require "#{Dir.pwd}/spec/oblisp/parser/parser.spec.rb"
 
-# require "#{Dir.pwd}/spec/oblisp/version.spec.rb";
+# oblisp
+require "#{Dir.pwd}/spec/oblisp/version.spec.rb";
