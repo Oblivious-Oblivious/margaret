e6837faf8e598b1ec51ca79c6cae85839ce578cf
Oblivious <thanos.papap42@gmail.com>
2022-07-06 19:37:12 +0300

    Fixed syntax inconsistencies.

--- spec/oblisp/parser/parser.spec.rb
+++ spec/oblisp/parser/parser.spec.rb
@@ -2,31 +2,30 @@ require_relative "_parser_helpers";
 
 describe Parser do
     it "creates a Parser object with Lexer as a parameter" do
-        l = Lexer.new "file.obl", "(42 factorial.)".chars;
+        l = Lexer.new "file.obl", "(42 factorial)".chars;
         p = Parser.new l;
         expect(p).to be_kind_of Parser;
     end
 
     it "has a nil token when initially creating the parser" do
-        l = Lexer.new "file.obl", "(42 factorial.)".chars;
+        l = Lexer.new "file.obl", "(42 factorial)".chars;
         p = Parser.new l;
         expect(p.token).to be nil;
     end
 
     it "traverses through the token table" do
-        l = Lexer.new "file.obl", "(42 factorial.)".chars;
+        l = Lexer.new "file.obl", "(42 factorial)".chars;
         l.make_tokens;
         p = Parser.new l;
         expect(p.next_token.value).to eq "(";
         expect(p.next_token.value).to eq "42";
         expect(p.next_token.value).to eq "factorial";
-        expect(p.next_token.value).to eq ".";
         expect(p.next_token.value).to eq ")";
         expect(p.next_token.value).to eq "eof";
     end
 
     it "peeks on top of the token table" do
-        l = Lexer.new "file.obl", "(42 factorial.)".chars;
+        l = Lexer.new "file.obl", "(42 factorial)".chars;
         l.make_tokens;
         p = Parser.new l;
         expect(p.peek_token.value).to eq "(";
@@ -44,7 +43,7 @@ describe Parser do
     end
 
     it "has an error message for syntax errors" do
-        l = Lexer.new "file.obl", "(42 factorial.)".chars;
+        l = Lexer.new "file.obl", "(42 factorial)".chars;
         l.make_tokens;
         p = Parser.new l;
         p.next_token;
