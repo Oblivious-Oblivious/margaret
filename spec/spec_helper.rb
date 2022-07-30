38f0cb7eeb91e77f9cdf9a1ef88c2d2d24c6ac1c
Oblivious <thanos.papap42@gmail.com>
2022-07-30 15:41:08 +0300

    Moved tests to own directory.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,47 +1,47 @@
 require "rspec/autorun";
 
 # All source files
-Dir["#{Dir.pwd}/src/oblisp/**/*.rb"].each { |f| require f };
+Dir["#{Dir.pwd}/src/interpreter/**/*.rb"].each { |f| require f };
 
 # oblisp
-require_relative "oblisp/version.spec";
+require_relative "interpreter/version.spec";
 
 # ast
-require_relative "oblisp/ast/ASTFactory.spec";
-require_relative "oblisp/ast/ast_types/Default.spec";
-require_relative "oblisp/ast/ast_types/SExpression.spec";
+require_relative "interpreter/ast/ASTFactory.spec";
+require_relative "interpreter/ast/ast_types/Default.spec";
+require_relative "interpreter/ast/ast_types/SExpression.spec";
 
 # lexer
-require_relative "oblisp/lexer/FileLoader.spec";
-require_relative "oblisp/lexer/Lexer.spec";
+require_relative "interpreter/lexer/FileLoader.spec";
+require_relative "interpreter/lexer/Lexer.spec";
 
 # parser
-require_relative "oblisp/parser/Parser.spec";
-require_relative "oblisp/parser/arithmetic.spec";
-require_relative "oblisp/parser/assignments.spec";
-require_relative "oblisp/parser/bitwise.spec";
-require_relative "oblisp/parser/blocks.spec";
-require_relative "oblisp/parser/booleans.spec";
-require_relative "oblisp/parser/characters.spec";
-require_relative "oblisp/parser/classes.spec";
-require_relative "oblisp/parser/code_as_data.spec";
-require_relative "oblisp/parser/conditionals.spec";
-require_relative "oblisp/parser/constants.spec";
-require_relative "oblisp/parser/conversions.spec";
-require_relative "oblisp/parser/date_time.spec";
-require_relative "oblisp/parser/hashes.spec";
-require_relative "oblisp/parser/identifiers.spec";
-require_relative "oblisp/parser/intervals.spec";
-require_relative "oblisp/parser/iterations.spec";
-require_relative "oblisp/parser/lists.spec";
-require_relative "oblisp/parser/messages.spec";
-require_relative "oblisp/parser/meta_evaluation.spec";
-require_relative "oblisp/parser/numeric.spec";
-require_relative "oblisp/parser/strings.spec";
-require_relative "oblisp/parser/symbols.spec";
-require_relative "oblisp/parser/tuples.spec";
+require_relative "interpreter/parser/Parser.spec";
+require_relative "interpreter/parser/arithmetic.spec";
+require_relative "interpreter/parser/assignments.spec";
+require_relative "interpreter/parser/bitwise.spec";
+require_relative "interpreter/parser/blocks.spec";
+require_relative "interpreter/parser/booleans.spec";
+require_relative "interpreter/parser/characters.spec";
+require_relative "interpreter/parser/classes.spec";
+require_relative "interpreter/parser/code_as_data.spec";
+require_relative "interpreter/parser/conditionals.spec";
+require_relative "interpreter/parser/constants.spec";
+require_relative "interpreter/parser/conversions.spec";
+require_relative "interpreter/parser/date_time.spec";
+require_relative "interpreter/parser/hashes.spec";
+require_relative "interpreter/parser/identifiers.spec";
+require_relative "interpreter/parser/intervals.spec";
+require_relative "interpreter/parser/iterations.spec";
+require_relative "interpreter/parser/lists.spec";
+require_relative "interpreter/parser/messages.spec";
+require_relative "interpreter/parser/meta_evaluation.spec";
+require_relative "interpreter/parser/numeric.spec";
+require_relative "interpreter/parser/strings.spec";
+require_relative "interpreter/parser/symbols.spec";
+require_relative "interpreter/parser/tuples.spec";
 
 # tokens
-require_relative "oblisp/tokens/Token.spec";
-require_relative "oblisp/tokens/TokenTable.spec";
-require_relative "oblisp/tokens/Type.spec";
+require_relative "interpreter/tokens/Token.spec";
+require_relative "interpreter/tokens/TokenTable.spec";
+require_relative "interpreter/tokens/Type.spec";
