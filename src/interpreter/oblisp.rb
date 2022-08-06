28d0dba48eeaf51c5238de1cb084f79fbed07d02
Oblivious <thanos.papap42@gmail.com>
2022-08-07 02:06:38 +0300

    Create oblisp.rb

---
+++ src/interpreter/oblisp.rb
@@ -0,0 +1,63 @@
+Dir[Dir.pwd + "/src/interpreter/**/*.rb"].each { |f| require f };
+
+require "readline";
+
+module TRANSLATE_TO
+    INTERPRETER = 1;
+    STACK_VM = 2;
+    REGISTER_VM = 3;
+    MACHINE_CODE = 4;
+end
+
+class Oblisp
+    def self.SCAN(prompt)
+        if line = Readline.readline(prompt, true)
+            if line == ""
+                ['(', ')'];
+            else
+                line.chars;
+            end
+        else
+            nil;
+        end
+    end
+
+    def self.READ(chars)
+        Lexer.new("repl", chars).make_tokens;
+    end
+
+    # TODO def self.EVAL(ast) -> AST traversal
+    def self.EVAL(tokens)
+        Parser.new(tokens).analyse_syntax;
+    end
+
+    def self.PRINT(evaluated)
+        puts "=> #{evaluated}";
+    end
+
+    def self.repl(type)
+        case type
+        when TRANSLATE_TO::INTERPRETER
+            loop { PRINT(EVAL(READ(SCAN("$> ")))); };
+        when TRANSLATE_TO::STACK_VM
+            puts "Stack VM";
+        when TRANSLATE_TO::REGISTER_VM
+            puts "Register VM";
+        when TRANSLATE_TO::MACHINE_CODE
+            puts "Machine Code";
+        else
+            puts "error";
+        end
+    end
+
+    def self.run_file(filename)
+        chars = FileLoader.new(filename).load;
+        tokens = READ(chars);
+        evaluated = EVAL(tokens);
+
+        puts evaluated;
+    end
+end
+
+# Oblisp.repl TRANSLATE_TO::INTERPRETER;
+Oblisp.run_file ARGV[0];
