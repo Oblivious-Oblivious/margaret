1a08e1c5aa285f63db5da8cf679081149aa66edc
Oblivious <thanos.papap42@gmail.com>
2022-09-16 15:45:57 +0300

    Simplified REPL interpreter and scanner.

--- margaret.rb
+++ margaret.rb
@@ -2,26 +2,16 @@ Dir[Dir.pwd + "/src/**/*.rb"].each { |f| require f };
 
 require "readline";
 
-module TRANSLATE_TO
-    INTERPRETER = 1;
-    STACK_VM = 2;
-    REGISTER_VM = 3;
-    MACHINE_CODE = 4;
-end
-
 class Margaret
     def self.SCAN(prompt)
-        if line = Readline.readline(prompt, true)
-            if line == ""
-                Readline::HISTORY.pop;
-                ['(', ')'];
-            elsif line == "<<exit>>" or line == "<<quit>>"
-                exit;
-            else
-                line.chars;
-            end
+        line = Readline.readline(prompt, true)
+        if line == ""
+            Readline::HISTORY.pop;
+            ['(', ')'];
+        elsif line == "<<exit>>" or line == "<<quit>>"
+            exit;
         else
-            nil;
+            line.chars;
         end
     end
 
@@ -29,7 +19,6 @@ class Margaret
         Lexer.new(filename, chars).make_tokens;
     end
 
-    # TODO def self.EVAL(ast) -> AST traversal
     def self.EVAL(tokens)
         Parser.new(tokens).analyse_syntax;
     end
@@ -38,19 +27,8 @@ class Margaret
         puts "=> #{evaluated}";
     end
 
-    def self.repl(type)
-        case type
-        when TRANSLATE_TO::INTERPRETER
-            loop { PRINT(EVAL(READ(SCAN("$> ")))); };
-        when TRANSLATE_TO::STACK_VM
-            puts "Stack VM";
-        when TRANSLATE_TO::REGISTER_VM
-            puts "Register VM";
-        when TRANSLATE_TO::MACHINE_CODE
-            puts "Machine Code";
-        else
-            puts "error";
-        end
+    def self.repl
+        loop { PRINT(EVAL(READ(SCAN("$> ")))); };
     end
 
     def self.run_file(filename)
@@ -62,5 +40,5 @@ class Margaret
     end
 end
 
-Margaret.repl TRANSLATE_TO::INTERPRETER;
+Margaret.repl;
 # Margaret.run_file ARGV[0];
