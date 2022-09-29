85183dfe85e0d4a0ed07c7c12c5d8bf358724814
Oblivious <thanos.papap42@gmail.com>
2022-09-29 18:00:40 +0300

    Removed the need for flattening arrays.
    Could be better solved with static types.

--- src/ast/Bytecode.rb
+++ src/ast/Bytecode.rb
@@ -4,19 +4,19 @@ class Bytecode
     end
 
     def first_unit(unit)
-        unit = unit.flatten;
+        unit;
     end
 
     def translation_unit(optional_assignment_list, expr)
-        res = [];
-        res << expr;
-        while optional_assignment_list.size > 0
-            res << optional_assignment_list.pop;
+        res = expr;
+        (optional_assignment_list.size-1).downto 0 do |i|
+            item = optional_assignment_list[i];
+            item.each { |opcode| res << opcode };
         end
         res;
     end
 
-    def assignment(id, eq)
+    def assignment(id, _)
         if id[0] == "push_instance"
             ["store_instance", "#{id[1]}"];
         else
@@ -30,9 +30,11 @@ class Bytecode
     end
 
     def unary_message(object, selectors)
-        res = object;
+        res = [];
+        object.each { |opcode| res << opcode };
         selectors.each do |sel|
-            res << "unary" << sel;
+            res << "unary";
+            sel.each { |opcode| res << opcode };
         end
         res;
     end
@@ -46,9 +48,11 @@ class Bytecode
     end
 
     def binary_message(object, selectors)
-        res = object;
+        res = [];
+        object.each { |opcode| res << opcode };
         selectors.each do |sel|
-            res << sel[1] << "binary" << sel[0];
+            sel[1].each { |opcode| res << opcode };
+            res << "binary" << sel[0];
         end
         res;
     end
@@ -58,18 +62,32 @@ class Bytecode
     end
 
     def binary_selector(sel, obj)
-        [sel, obj];
+        res = [];
+        res << sel;
+        res << obj;
+        res;
     end
 
     def keyword_message(object, selectors)
         if selectors.size > 1 and selectors.all? { |sel| sel.first == selectors.first.first }
-            selectors.map { |sel| [object, sel[1], "keyword", sel[0], "1"] };
+            res = [];
+            selectors.each do |sel|
+                object.each { |opcode| res << opcode };
+                sel[1].each { |opcode| res << opcode };
+                res << "keyword" << sel[0] << "1";
+            end
+            res;
         else
             joined_selector = "";
             selectors.each { |sel| joined_selector << sel[0] };
-            res = object;
-            selectors.each { |sel| res << sel[1] };
+
+            res = [];
+            object.each { |opcode| res << opcode };
+            selectors.each do |sel|
+                sel[1].each { |opcode| res << opcode };
+            end
             res << "keyword" << joined_selector << "#{selectors.size}";
+            res;
         end
     end
 
@@ -86,7 +104,11 @@ class Bytecode
     end
 
     def group(unit_list)
-        unit_list;
+        res = [];
+        unit_list.each do |unit|
+            unit.each { |opcode| res << opcode };
+        end
+        res;
     end
 
     def variable(optional_instance_symbol, name)
@@ -109,28 +131,45 @@ class Bytecode
 
     # TODO New activation window on procs and methods
     def proc_literal(param_list, function)
-        param_list = param_list.map { |item| ["push_variable", item] };
-        ["STARTpush_proc", param_list, "push_tensor", "#{param_list.size}", function, "ENDpush_proc"];
+        res = ["STARTpush_proc"];
+        param_list.each { |item| res << "push_variable" << item };
+        res << "push_tensor" << "#{param_list.size}";
+        function.each { |item| res << item };
+        res << "ENDpush_proc";
+        res;
     end
 
     def c_function_declaration(return_type, name, params)
-        params = params.map { |param| ["push_variable", "CFunParam", "push_variable", "#{param[0]}", "push_variable", "#{param[1]}", "keyword", "c_type:c_name:", "2"] };
-        ["STARTpush_c_function", "push_variable", return_type, "push_variable", name, params, "push_tensor", "#{params.size}", "ENDpush_c_function"];
+        res = ["STARTpush_c_function", "push_variable", return_type, "push_variable", name];
+        params.each { |item| res << "push_variable" << "CFunParam" << "push_variable" << "#{item[0]}" << "push_variable" << "#{item[1]}" << "keyword" << "c_type:c_name:" << "2" };
+        res << "push_tensor" << "#{params.size}" << "ENDpush_c_function";
+        res;
     end
     
     def unary_method_definition(selector, function)
-        ["STARTpush_unary_method", %Q{"#{selector}"}, function, "ENDpush_unary_method"];
+        res = ["STARTpush_unary_method", %Q{"#{selector}"}];
+        function.each { |item| res << item };
+        res << "ENDpush_unary_method";
+        res;
     end
 
     def binary_method_definition(selector, param, function)
-        ["STARTpush_binary_method", %Q{"#{selector}"}, "push_variable", param, function, "ENDpush_binary_method"];
+        res = ["STARTpush_binary_method", %Q{"#{selector}"}, "push_variable", param];
+        function.each { |item| res << item };
+        res << "ENDpush_binary_method";
+        res;
     end
     
     def keyword_method_definition(selector, function)
         joined_selector = "";
         selector.each { |sel| joined_selector << sel[0] };
-        params = selector.map { |item| ["push_variable", item[1]] };
-        ["STARTpush_keyword_method", %Q{"#{joined_selector}"}, params, "push_tensor", "#{params.size}", function, "ENDpush_keyword_method"];
+
+        res = ["STARTpush_keyword_method", %Q{"#{joined_selector}"}];
+        selector.each { |sel| res << "push_variable" << sel[1] };
+        res << "push_tensor" << "#{selector.size}";
+        function.each { |item| res << item };
+        res << "ENDpush_keyword_method";
+        res;
     end
 
     def literal(unit)
@@ -180,18 +219,37 @@ class Bytecode
     end
 
     def tensor_literal(item_list)
-        [item_list, "push_tensor", "#{item_list.size}"];
+        res = [];
+        item_list.each do |item|
+            item.each { |opcode| res << opcode };
+        end
+        res << "push_tensor" << "#{item_list.size}";
+        res;
     end
 
     def hash_literal(association_list)
-        [association_list, "push_hash", "#{association_list.size}"];
+        res = [];
+        association_list.each do |item|
+            item.each { |opcode| res << opcode };
+        end
+        res << "push_hash" << "#{association_list.size}";
+        res;
     end
 
     def association(key, value)
-        ["push_variable", "Association", key, value, "keyword", "key:value:", "2"];
+        res = [];
+        res << "push_variable" << "Association";
+        key.each { |opcode| res << opcode };
+        value.each { |opcode| res << opcode };
+        res << "keyword" << "key:value:" << "2";
+        res;
     end
 
     def json_association(key, value)
-        ["push_variable", "Association", "push_string", %Q{"#{key}"}, value, "keyword", "key:value:", "2"];
+        res = [];
+        res << "push_variable" << "Association" << "push_string" << %Q{"#{key}"};
+        value.each { |opcode| res << opcode };
+        res << "keyword" << "key:value:" << "2";
+        res;
     end
 end
