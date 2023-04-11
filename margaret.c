054e15105be55e6933a85a2c4ed4559f7e95c6fb
Oblivious <thanos.papap42@gmail.com>
2023-04-12 01:16:03 +0300

    Update margaret.c

--- margaret.c
+++ margaret.c
@@ -1,6 +1,4 @@
 // Copyright (C) 2023 Thanasis Papapostolou (Oblivious)
-// See end of file for copyright information
-// This file is part of margaret.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -83,15 +81,3 @@ int main(int argc, char **argv) {
         margaret_run_file(argv[1]);
     }
 }
-
-// Copyright (C) 2023 Thanasis Papapostolou (Oblivious)
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
