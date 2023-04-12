f1ff152950b3c67910747411e7b36035df137c08
Oblivious <thanos.papap42@gmail.com>
2023-04-12 22:22:27 +0300

    Added back the `debug` helper.

--- spec/_helpers.h
+++ spec/_helpers.h
@@ -23,6 +23,31 @@
             assert_that_charptr(marg_string_get(marg_vector_get(res, i)) equals to marg_string_get(marg_vector_get((expected_result), i))); \
 } while(0)
 
+#define debug(code, expected_result) do { \
+    Lexer *l = lexer_new("file.marg", marg_string_new((code))); \
+    Parser *p = parser_new(lexer_make_tokens(l)); \
+    marg_vector *res = parser_analyze_syntax(p); \
+    size_t size_of_res = marg_vector_size(res); \
+    size_t the_same_size_as_expected = marg_vector_size((expected_result)); \
+    printf("["); \
+    for(size_t i = 0; i < size_of_res-1; i++) { \
+        if(res == NULL || marg_vector_get(res, i) == NULL or marg_string_get(marg_vector_get(res, i)) == NULL) \
+            printf("(NULL), "); \
+        else \
+            printf("%s, ", marg_string_get(marg_vector_get(res, i))); \
+    } \
+    if(res == NULL || marg_vector_get(res, size_of_res-1) == NULL or marg_string_get(marg_vector_get(res, size_of_res-1)) == NULL) \
+        printf("(NULL), "); \
+    else \
+        printf("%s]\n", marg_string_get(marg_vector_get(res, size_of_res-1))); \
+    \
+    assert_that_int(size_of_res equals to the_same_size_as_expected); \
+    \
+    if(size_of_res <= the_same_size_as_expected) \
+        for(size_t i = 0; i < size_of_res; i++) \
+            assert_that_charptr(marg_string_get(marg_vector_get(res, i)) equals to marg_string_get(marg_vector_get((expected_result), i))); \
+} while(0)
+
 #define error(code, error_message) do { \
     Lexer *l = lexer_new("file.marg", marg_string_new((code))); \
     Parser *p = parser_new(lexer_make_tokens(l)); \
