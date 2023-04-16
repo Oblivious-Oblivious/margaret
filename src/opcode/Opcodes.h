759ff2e65f258a53aee82b9c6467db8620df9946
Oblivious <thanos.papap42@gmail.com>
2023-04-16 20:09:34 +0300

    Update Opcodes.h

--- src/opcode/Opcodes.h
+++ src/opcode/Opcodes.h
@@ -7,18 +7,22 @@
 #define OP_NIL               marg_string_new("NIL")
 #define OP_TRUE              marg_string_new("TRUE")
 #define OP_FALSE             marg_string_new("FALSE")
+
 #define OP_SELF              marg_string_new("SELF")
 #define OP_SUPER             marg_string_new("SUPER")
+
 #define OP_0                 marg_string_new("0")
 #define OP_1                 marg_string_new("1")
 #define OP_MINUS_1           marg_string_new("MINUS_1")
 #define OP_2                 marg_string_new("2")
+
+#define OP_VARIABLE          marg_string_new("VARIABLE")
+#define OP_INSTANCE          marg_string_new("INSTANCE")
+
 #define OP_INTEGER           marg_string_new("INTEGER")
 #define OP_FLOAT             marg_string_new("FLOAT")
 #define OP_CHAR              marg_string_new("CHAR")
 #define OP_STRING            marg_string_new("STRING")
-#define OP_VARIABLE          marg_string_new("VARIABLE")
-#define OP_INSTANCE          marg_string_new("INSTANCE")
 #define OP_TENSOR            marg_string_new("TENSOR")
 #define OP_TUPLE             marg_string_new("TUPLE")
 #define OP_BITSTRING         marg_string_new("BITSTRING")
@@ -35,14 +39,13 @@
 #define OP_STORE             marg_string_new("STORE")
 #define OP_STORE_INSTANCE    marg_string_new("STORE_INSTANCE")
 
-#define OP_UNARY             marg_string_new("UNARY")
 #define OP_METHOD_UNARY      marg_string_new("METHOD_UNARY")
-
-#define OP_BINARY            marg_string_new("BINARY")
 #define OP_METHOD_BINARY     marg_string_new("METHOD_BINARY")
-
-#define OP_KEYWORD           marg_string_new("KEYWORD")
 #define OP_METHOD_KEYWORD    marg_string_new("METHOD_KEYWORD")
+
+#define OP_UNARY             marg_string_new("UNARY")
+#define OP_BINARY            marg_string_new("BINARY")
+#define OP_KEYWORD           marg_string_new("KEYWORD")
 
 // ------ Emmited Opcodes ------
 // #define OP_DECLARE_VARIABLE              marg_string_new("DECLARE_VARIABLE")
