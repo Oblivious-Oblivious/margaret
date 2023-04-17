e544f8daa08c09ca96df7bc38ad8fb594be241b8
Oblivious <thanos.papap42@gmail.com>
2023-04-18 00:04:05 +0300

    Added helpers for splitting and joinning a 32 bit value into distinct bytes.

--- src/opcode/MargValue.c
+++ src/opcode/MargValue.c
@@ -1,7 +1,27 @@
 #include "MargValue.h"
 
+#include "../base/memory.h"
+
 marg_string *marg_value_format(MargValue self) {
     marg_string *res = marg_string_new("");
     marg_string_addf(res, "%g", self);
     return res;
+}
+
+uint8_t *long_constant_to_bytes(uint32_t constant) {
+    uint8_t *res = (uint8_t*)collected_malloc(sizeof(uint8_t) * 4);
+
+    uint16_t first = (constant >> 16) & 0xffff;
+    uint16_t second = constant & 0xffff;
+
+    res[0] = (first >> 8) & 0xff;
+    res[1] = first & 0xff;
+    res[2] = (second >> 8) & 0xff;
+    res[3] = second & 0xff;
+
+    return res;
+}
+
+uint32_t bytes_to_long_constant(uint8_t *bytes) {
+    return ((bytes[0] << 8) | bytes[1] << 8) | (bytes[2] << 8) | bytes[3];
 }
--- src/opcode/MargValue.h
+++ src/opcode/MargValue.h
@@ -5,6 +5,25 @@
 
 typedef double MargValue;
 
+/**
+ * @brief Formats a marg value using QNAN boxing
+ * @param self -> The MargValue represented as a double
+ * @return marg_string*
+ */
 marg_string *marg_value_format(MargValue self);
+
+/**
+ * @brief Splits a 32 bit constant into 4, 8-bit parts
+ * @param constant -> The 32 bit value
+ * @return uint8_t* -> The 4*8 bit vector
+ */
+uint8_t *long_constant_to_bytes(uint32_t constant);
+
+/**
+ * @brief Joins 4, 8-bit parts into a single 32 bit integer
+ * @param bytes -> The list of bytes
+ * @return uint32_t -> The 32 bit result
+ */
+uint32_t bytes_to_long_constant(uint8_t *bytes);
 
 #endif
