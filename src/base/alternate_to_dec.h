c516fe053bbda0b2d8792ca51c6c4357b56a2fa4
Oblivious <thanos.papap42@gmail.com>
2023-04-21 15:42:05 +0300

    Update alternate_to_dec.h

--- src/base/alternate_to_dec.h
+++ src/base/alternate_to_dec.h
@@ -1,5 +1,9 @@
 #include <stdlib.h> /* uint64_t */
 
+/**
+ * @brief Converts character representations
+    of non base 10 numbers into base 10 ones
+ */
 char *bin_to_dec(char *bin);
 char *oct_to_dec(char *oct);
 char *hex_to_dec(char *hex);
