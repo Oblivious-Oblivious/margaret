3add76bacc2caf2a3c08d767e285a048efa3a2d0
Oblivious <thanos.papap42@gmail.com>
2023-04-27 00:45:38 +0300

    Added skeleton spec files.

---
+++ spec/opcode/MargInteger.spec.h
@@ -0,0 +1,10 @@
+#ifndef __MARG_INTEGER_SPEC_H_
+#define __MARG_INTEGER_SPEC_H_
+
+#include "../_helpers.h"
+
+module(MargIntegerSpec, {
+    // TODO
+})
+
+#endif
---
+++ spec/opcode/MargNil.spec.h
@@ -0,0 +1,10 @@
+#ifndef __MARG_NIL_SPEC_H_
+#define __MARG_NIL_SPEC_H_
+
+#include "../_helpers.h"
+
+module(MargNilSpec, {
+    // TODO
+})
+
+#endif
