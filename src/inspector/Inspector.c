7a0229feb7e73531ecf3150024d65adc024062bf
Oblivious <thanos.papap42@gmail.com>
2023-04-26 13:51:54 +0300

    Renamed disassembly instructions to be more descriptive.

--- src/inspector/Inspector.c
+++ src/inspector/Inspector.c
@@ -1,6 +1,7 @@
 #include "Inspector.h"
 
-#include <stdio.h>
+#include <stdio.h> /* printf */
+#include <string.h> /* strcmp */
 
 #include "../base/string.h"
 
@@ -22,7 +23,7 @@ static void write_offset_and_line_number_on(string *disassembled_instruction, Ch
  * @param offset -> Current offset
  * @return size_t -> Newly calculated offset
  */
-static size_t instruction_offset_1(vector *res, const char *name, Chunk *chunk, size_t offset) {
+static size_t instruction_single(vector *res, const char *name, Chunk *chunk, size_t offset) {
     uint8_t opcode = chunk_get(chunk, offset);
 
     string *disassembled_instruction = string_new("");
@@ -34,7 +35,7 @@ static size_t instruction_offset_1(vector *res, const char *name, Chunk *chunk, 
     return offset + 1;
 }
 
-static size_t instruction_offset_2(vector *res, const char *name, Chunk *chunk, size_t offset) {
+static size_t instruction_constant(vector *res, const char *name, Chunk *chunk, size_t offset) {
     uint8_t opcode = chunk_get(chunk, offset);
     uint8_t constant = chunk_get(chunk, offset + 1);
 
@@ -49,7 +50,7 @@ static size_t instruction_offset_2(vector *res, const char *name, Chunk *chunk, 
     return offset + 2;
 }
 
-static size_t instruction_offset_5(vector *res, const char *name, Chunk *chunk, size_t offset) {
+static size_t instruction_long_constant(vector *res, const char *name, Chunk *chunk, size_t offset) {
     uint8_t opcode = chunk_get(chunk, offset);
     uint8_t bytes[4] = {
         chunk_get(chunk, offset + 1),
@@ -81,22 +82,22 @@ static size_t inspect_instruction(vector *res, Chunk *chunk, size_t offset) {
     uint8_t instruction = chunk_get(chunk, offset);
     switch(instruction) {
         case OP_RETURN:
-            return instruction_offset_1(res, "RETURN", chunk, offset);
+            return instruction_single(res, "RETURN", chunk, offset);
 
         case OP_NIL:
-            return instruction_offset_1(res, "NIL", chunk, offset);
+            return instruction_single(res, "NIL", chunk, offset);
         case OP_TRUE:
-            return instruction_offset_1(res, "TRUE", chunk, offset);
+            return instruction_single(res, "TRUE", chunk, offset);
         case OP_FALSE:
-            return instruction_offset_1(res, "FALSE", chunk, offset);
+            return instruction_single(res, "FALSE", chunk, offset);
 
         case OP_CONSTANT:
-            return instruction_offset_2(res, "CONSTANT", chunk, offset);
+            return instruction_constant(res, "CONSTANT", chunk, offset);
         case OP_CONSTANT_LONG:
-            return instruction_offset_5(res, "CONSTANT_LONG", chunk, offset);
+            return instruction_long_constant(res, "CONSTANT_LONG", chunk, offset);
 
         case OP_POP:
-            return instruction_offset_1(res, "POP", chunk, offset);
+            return instruction_single(res, "POP", chunk, offset);
 
         case OP_STORE_GLOBAL:
             return instruction_offset_2(res, "STORE_GLOBAL", chunk, offset);
