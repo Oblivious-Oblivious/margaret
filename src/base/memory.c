ee41b16bcc9bbfabe3a948827d2a7a10e1b285c7
Oblivious <thanos.papap42@gmail.com>
2023-04-28 12:54:24 +0300

    Removed automated garbage collector.
    TODO -> Implement a custom version.

--- src/base/memory.c
+++ src/base/memory.c
@@ -2,10 +2,14 @@
 
 #include "../../libs/boehm_gc/bdwgc/gc.h"
 
+// TODO Implement a rudimentary GC
+
 void *collected_malloc(size_t nbytes) {
-    return GC_MALLOC(nbytes);
+    return malloc(nbytes);
+    // return GC_MALLOC(nbytes);
 }
 
 void *collected_realloc(void *old, size_t new_size) {
-    return GC_REALLOC(old, new_size);
+    return realloc(old, new_size);
+    // return GC_REALLOC(old, new_size);
 }
