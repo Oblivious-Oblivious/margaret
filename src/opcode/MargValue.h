63b54a7b90a561cb11c468803a514f1ea6d53075
Oblivious <thanos.papap42@gmail.com>
2023-04-29 23:11:20 +0300

    Enclosed macros in parentheses.

--- src/opcode/MargValue.h
+++ src/opcode/MargValue.h
@@ -20,17 +20,17 @@ typedef uint64_t MargValue;
 #define QNAN_BOX(pointer)                            (MargValue)(SIGN_BIT | QNAN | (uint64_t)(uintptr_t)(pointer))
 #define QNAN_UNBOX(value)                            ((MargPointer*)(uintptr_t)((value) & ~(SIGN_BIT | QNAN)))
 
-#define MARG_NIL                                     QNAN_BOX(marg_nil_new())
-#define MARG_FALSE                                   QNAN_BOX(marg_false_new())
-#define MARG_TRUE                                    QNAN_BOX(marg_true_new())
-#define MARG_INTEGER(number)                         QNAN_BOX(marg_integer_new((number)))
-#define MARG_MINUS_1                                 QNAN_BOX(marg_integer_new(-1))
-#define MARG_0                                       QNAN_BOX(marg_integer_new(0))
-#define MARG_1                                       QNAN_BOX(marg_integer_new(1))
-#define MARG_2                                       QNAN_BOX(marg_integer_new(2))
-#define MARG_FLOAT(number)                           QNAN_BOX(marg_float_new((number)))
-#define MARG_STRING(chars, size)                     QNAN_BOX(marg_string_new((chars), (size)))
-#define MARG_STRING_INTERNED(vm, chars, size, hash)  QNAN_BOX(table_find_string(&(vm)->interned_strings, (chars), (size), (hash)));
+#define MARG_NIL                                     (QNAN_BOX(marg_nil_new()))
+#define MARG_FALSE                                   (QNAN_BOX(marg_false_new()))
+#define MARG_TRUE                                    (QNAN_BOX(marg_true_new()))
+#define MARG_INTEGER(number)                         (QNAN_BOX(marg_integer_new((number))))
+#define MARG_MINUS_1                                 (QNAN_BOX(marg_integer_new(-1)))
+#define MARG_0                                       (QNAN_BOX(marg_integer_new(0)))
+#define MARG_1                                       (QNAN_BOX(marg_integer_new(1)))
+#define MARG_2                                       (QNAN_BOX(marg_integer_new(2)))
+#define MARG_FLOAT(number)                           (QNAN_BOX(marg_float_new((number))))
+#define MARG_STRING(chars, size)                     (QNAN_BOX(marg_string_new((chars), (size))))
+#define MARG_STRING_INTERNED(vm, chars, size, hash)  (QNAN_BOX(table_find_string(&(vm)->interned_strings, (chars), (size), (hash))))
 
 #define AS_NIL(value)                                ((MargNil*)QNAN_UNBOX(value))
 #define AS_FALSE(value)                              ((MargFalse*)QNAN_UNBOX(value))
@@ -39,12 +39,12 @@ typedef uint64_t MargValue;
 #define AS_FLOAT(value)                              ((MargFloat*)QNAN_UNBOX(value))
 #define AS_STRING(value)                             ((MargString*)QNAN_UNBOX(value))
 
-#define IS_NIL(value)                                QNAN_UNBOX(value)->type == MARG_NIL_TYPE
-#define IS_FALSE(value)                              QNAN_UNBOX(value)->type == MARG_FALSE_TYPE
-#define IS_TRUE(value)                               QNAN_UNBOX(value)->type == MARG_TRUE_TYPE
-#define IS_INTEGER(value)                            QNAN_UNBOX(value)->type == MARG_INTEGER_TYPE
-#define IS_FLOAT(value)                              QNAN_UNBOX(value)->type == MARG_FLOAT_TYPE
-#define IS_STRING(value)                             QNAN_UNBOX(value)->type == MARG_STRING_TYPE
+#define IS_NIL(value)                                (QNAN_UNBOX(value)->type == MARG_NIL_TYPE)
+#define IS_FALSE(value)                              (QNAN_UNBOX(value)->type == MARG_FALSE_TYPE)
+#define IS_TRUE(value)                               (QNAN_UNBOX(value)->type == MARG_TRUE_TYPE)
+#define IS_INTEGER(value)                            (QNAN_UNBOX(value)->type == MARG_INTEGER_TYPE)
+#define IS_FLOAT(value)                              (QNAN_UNBOX(value)->type == MARG_FLOAT_TYPE)
+#define IS_STRING(value)                             (QNAN_UNBOX(value)->type == MARG_STRING_TYPE)
 
 /**
  * @brief Formats a marg value using QNAN boxing
