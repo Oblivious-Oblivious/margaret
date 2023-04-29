efff53551061a11ea1dcf7f2acc95afc354f8602
Oblivious <thanos.papap42@gmail.com>
2023-04-29 04:35:11 +0300

    Removed redundant test.

--- spec/inspector/Inspector.spec.h
+++ spec/inspector/Inspector.spec.h
@@ -8,32 +8,6 @@
 
 module(InspectorSpec, {
     // TODO Test for a `postcard` type mini program that includes every single feature
-
-    // for(size_t i = 0; i < vector_size(res); i++)
-    //     printf("%s\n", string_get(vector_get(res, i)));
-
-    it("prints a list of emmitted opcodes", {
-        VM *vm = vm_new();
-        Chunk *bytecode = vm->bytecode;
-
-        uint8_t constant = chunk_constant_add(bytecode, MARG_FLOAT(0.7));
-        chunk_add(bytecode, OP_CONSTANT, 123);
-        chunk_add(bytecode, constant, 123);
-        constant = chunk_constant_add(bytecode, MARG_FLOAT(0.8));
-        chunk_add(bytecode, OP_CONSTANT, 123);
-        chunk_add(bytecode, constant, 123);
-        constant = chunk_constant_add(bytecode, MARG_FLOAT(0.9));
-        chunk_add(bytecode, OP_CONSTANT, 123);
-        chunk_add(bytecode, constant, 123);
-        chunk_add(bytecode, OP_RETURN, 123);
-
-        vector *res = inspect_vm_bytecode(vm);
-        assert_that_charptr(string_get(vector_get(res, 0)) equals to "0000       123      04 00               CONSTANT                 0 (0.7)");
-        assert_that_charptr(string_get(vector_get(res, 1)) equals to "0002         |      04 01               CONSTANT                 1 (0.8)");
-        assert_that_charptr(string_get(vector_get(res, 2)) equals to "0004         |      04 02               CONSTANT                 2 (0.9)");
-        assert_that_charptr(string_get(vector_get(res, 3)) equals to "0006         |      00                  RETURN");
-    });
-
     it("tests multiple long temporaries", {
         VM *vm = vm_new();
         Chunk *bytecode = vm->bytecode;
