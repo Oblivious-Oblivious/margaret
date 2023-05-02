4b005e2c5a0a5c8a9b91b21945657a73f8ec3067
Oblivious <thanos.papap42@gmail.com>
2023-05-02 17:10:03 +0300

    Refactored bit masks to be easier to understand.

--- src/opcode/MargValue.h
+++ src/opcode/MargValue.h
@@ -11,22 +11,23 @@ typedef struct VM VM;
 
 /* QNAN = 0b    0     11111111111       1            1       ('0' * 50)
              (sign) (exponent bits) (qnan bit) (qnan fp ind)   (rest)        */
-#define QNAN       ((uint64_t)0x7ffc000000000000)
 #define SIGN_BIT   ((uint64_t)0x8000000000000000)
+#define EXPONENTS  ((uint64_t)0x7ff0000000000000)
+#define QNAN       ((uint64_t)0x0008000000000000)
 
-#define QNAN_BOX(pointer)                            ((MargValue)(QNAN | (uint64_t)(uintptr_t)(pointer)))
-#define QNAN_UNBOX(value)                            ((MargObject*)(uintptr_t)((value) & ~(QNAN)))
+#define QNAN_BOX(pointer)                            ((MargValue)(EXPONENTS | QNAN | (uint64_t)(uintptr_t)(pointer)))
+#define QNAN_UNBOX(value)                            ((MargObject*)(uintptr_t)((value) & ~(EXPONENTS | QNAN)))
 
 // sign = 1
 // rest =  ('0' * 48)    0       0
 //          (Pointer) (tag1)  (tag0)
 // Allows for 4 distinct values (00, 01, 10, 11) that are not objects
 #define UNDEFINED_TAG                                0x00
-#define MARG_UNDEFINED                               ((MargValue)(uint64_t)(QNAN | SIGN_BIT | UNDEFINED_TAG))
+#define MARG_UNDEFINED                               ((MargValue)(uint64_t)(SIGN_BIT | EXPONENTS | QNAN | UNDEFINED_TAG))
 #define IS_UNDEFINED(value)                          ((value) == MARG_UNDEFINED)
 
 #define NOT_INTERNED_TAG                             0x01
-#define MARG_NOT_INTERNED                            ((MargValue)(uint64_t)(QNAN | SIGN_BIT | NOT_INTERNED_TAG))
+#define MARG_NOT_INTERNED                            ((MargValue)(uint64_t)(SIGN_BIT | EXPONENTS | QNAN | NOT_INTERNED_TAG))
 #define IS_NOT_INTERNED(value)                       ((value) == MARG_NOT_INTERNED)
 
 // Objects
