63d0dcd3b15ca6e1f61e26220679e4f50da39781
Oblivious <thanos.papap42@gmail.com>
2023-05-03 17:26:55 +0300

    Update procs.spec.h

--- spec/generic/procs.spec.h
+++ spec/generic/procs.spec.h
@@ -23,8 +23,8 @@ module(procs_spec, {
         parse("{ (x = 1, y = 2, x + y) }", vector_new(FM_START_PROC, FM_INTEGER, string_new("1"), FM_STORE_LOCAL, string_new("x"), FM_INTEGER, string_new("2"), FM_STORE_LOCAL, string_new("y"), FM_LOCAL, string_new("x"), FM_LOCAL, string_new("y"), FM_BINARY, string_new("+"), FM_END_PROC));
         parse("{ param | param puts }", vector_new(FM_START_PROC, FM_PROC_PARAMETER, string_new("param"), FM_LOCAL, string_new("param"), FM_UNARY, string_new("puts"), FM_END_PROC));
         parse("{ a | a }", vector_new(FM_START_PROC, FM_PROC_PARAMETER, string_new("a"), FM_LOCAL, string_new("a"), FM_END_PROC));
-        parse("{ x = 2 } eval", vector_new(FM_START_PROC, FM_INTEGER, string_new("2"), FM_STORE_LOCAL, string_new("x"), FM_END_PROC, FM_UNARY, string_new("eval")));
-        parse("{ param | param puts } value: 42", vector_new(FM_START_PROC, FM_PROC_PARAMETER, string_new("param"), FM_LOCAL, string_new("param"), FM_UNARY, string_new("puts"), FM_END_PROC, FM_INTEGER, string_new("42"), FM_KEYWORD, string_new("value:"), string_new("1")));
+        parse("{ x = 2 } call", vector_new(FM_START_PROC, FM_INTEGER, string_new("2"), FM_STORE_LOCAL, string_new("x"), FM_END_PROC, FM_UNARY, string_new("call")));
+        parse("{ param | param puts } call: {a: 42}", vector_new(FM_START_PROC, FM_PROC_PARAMETER, string_new("param"), FM_LOCAL, string_new("param"), FM_UNARY, string_new("puts"), FM_END_PROC, FM_LOCAL, string_new("Association"), FM_STRING, string_new("a"), FM_INTEGER, string_new("42"), FM_KEYWORD, string_new("key:value:"), string_new("2"), FM_HASH, string_new("1"), FM_KEYWORD, string_new("call:"), string_new("1")));
     });
 })
 
