506d16009427ea332484ccfb1430dfc7d028bfb5
Oblivious <thanos.papap42@gmail.com>
2023-05-03 04:36:56 +0300

    Method creates a new Proc.

--- src/vm/Margaret.h
+++ src/vm/Margaret.h
@@ -8,7 +8,6 @@
 static void primitive_1_margaret_main(VM *vm) {
     MargObject *object = AS_OBJECT(MARG_OBJECT("$Margaret", 10));
     MargMethod *method = AS_METHOD(MARG_METHOD(object));
-    method->proc = AS_PROC(MARG_PROC(method));
 
     table_set(&object->messages, MARG_STRING("main:", 6), QNAN_BOX(method));
     table_set(&vm->global_variables, MARG_STRING("$Margaret", 10), QNAN_BOX(object));
