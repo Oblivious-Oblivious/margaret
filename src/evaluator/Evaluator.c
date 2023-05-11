f44ed5d6d7253161683e620fac25ce6dd2c07d9d
Oblivious <thanos.papap42@gmail.com>
2023-05-11 18:50:49 +0300

    Removes any POP operations from print since we call this from unary and keywords
    Eventually only store `puts:`

--- src/evaluator/Evaluator.c
+++ src/evaluator/Evaluator.c
@@ -33,7 +33,7 @@ static void evaluator_run(VM *vm) {
             }
             case TEST_OP_PRINT: {
                 printf("%s\n", string_get(marg_value_format(STACK_PEEK(vm, 0))));
-                STACK_POP(vm);
+                // STACK_POP(vm);
                 // STACK_POP(vm);
                 break;
             }
