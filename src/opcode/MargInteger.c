f89b0037387ea1bba926eb3ecd17beb4a34f5c47
Oblivious <thanos.papap42@gmail.com>
2023-05-17 19:14:22 +0300

    Creating a new integer sets up the prototype delegation chain.

--- src/opcode/MargInteger.c
+++ src/opcode/MargInteger.c
@@ -1,8 +1,14 @@
 #include "MargInteger.h"
 
+#include "MargString.h"
+
 MargInteger *marg_integer_new(VM *vm, int64_t value) {
     MargObject *obj = (MargObject*)marg_object_new(vm, sizeof(MargInteger), "$Integer");
     MargInteger *self = (MargInteger*)obj;
+
+    MargValue proto_object = table_get(&vm->global_variables, MARG_STRING("$IntegerProto"));
+    obj->parent = AS_OBJECT(proto_object);
+    table_set(&obj->instance_variables, MARG_STRING("@super"), proto_object);
 
     self->value = value;
 
