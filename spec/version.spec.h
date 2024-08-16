822af67ae6e14884be39ae0d1c7b2843a56e8b96
oblivious <thanos.papap42@gmail.com>
2024-08-17 02:27:26 +0300

    Added new spec for city.

--- spec/version.spec.h
+++ spec/version.spec.h
@@ -2,19 +2,28 @@
 #define __VERSION_SPEC_H_
 
 #include "../libs/cSpec/export/cSpec.h"
+#include "../libs/EmeraldsOniguruma/export/EmeraldsOniguruma.h"
+#include "../libs/EmeraldsString/export/EmeraldsString.h"
+#include "../src/lexer/Lexer.h"
+#include "../src/parser/Parser.h"
 #include "../src/version.h"
+#include "../src/vm/vm.h"
 
 module(VersionSpec, {
-  it("checks that latest version number", {
+  it("validates version number", {
     assert_that_charptr(MARGARET_VERSION equals to "0.0.1");
   });
 
-  it("checks that latest date", {
+  it("validates date", {
     assert_that_charptr(MARGARET_DATE equals to "2022-2024");
   });
 
-  it("checks that latest link", {
+  it("validates link", {
     assert_that_charptr(MARGARET_LINK equals to "margaret.org");
+  });
+
+  it("validates city", {
+    assert_that_charptr(MARGARET_CITY equals to "Ioannina");
   });
 })
 
