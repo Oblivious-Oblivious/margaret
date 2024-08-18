fb42f8c2b3a27a4e8e9992ce285449880691639c
oblivious <thanos.papap42@gmail.com>
2024-08-18 03:49:46 +0300

    Added loader specs.

--- spec/loader/Loader.spec.h
+++ spec/loader/Loader.spec.h
@@ -4,11 +4,29 @@
 #include "../../libs/cSpec/export/cSpec.h"
 #include "../../src/loader/Loader.h"
 
-module(
-  LoaderSpec,
-  {
-    // TODO - Test for a generic file reader function.
-  }
-)
+module(LoaderSpec, {
+  it("loads text character by character", {
+    FILE *fd = fopen("test.marg", "w");
+    fprintf(fd, "%s", "text");
+    fclose(fd);
+
+    VM *vm = vm_new("test.marg");
+    loader_load(vm);
+
+    assert_that_int(vm->source[0] equals to 't');
+    assert_that_int(vm->source[1] equals to 'e');
+    assert_that_int(vm->source[2] equals to 'x');
+    assert_that_int(vm->source[3] equals to 't');
+    assert_that_charptr(vm->source equals to "text");
+  });
+
+  it("does not load non-existing file", {
+    VM *vm = vm_new("__this name does not exist__");
+    loader_load(vm);
+    assert_that(vm->source is NULL);
+  });
+
+  after({ remove("test.marg"); });
+})
 
 #endif
