e59c049ba6dbfa9da28d6cf8ac23cadecf491eb4
oblivious <thanos.papap42@gmail.com>
2024-09-14 02:56:05 +0300

    Simplified helper functions.

--- spec/generic/_helpers.h
+++ spec/generic/_helpers.h
@@ -8,46 +8,36 @@
 #include "../../src/opcode/fmcodes.h"
 #include "../../src/parser/Parser.h"
 
-#define parse(code, expected_result)                                          \
-  do {                                                                        \
-    VM *vm     = vm_new("file.marg");                                         \
-    vm->source = string_new(code);                                            \
-    lexer_make_tokens(vm);                                                    \
-    parser_analyze_syntax(vm);                                                \
-    char **res                     = vm->formal_bytecode;                     \
-    size_t size_of_formal_bytecode = vector_size(res);                        \
-    size_t same_size_as_expected   = vector_size(expected_result);            \
-    assert_that_int(size_of_formal_bytecode equals to same_size_as_expected); \
-                                                                              \
-    if(size_of_formal_bytecode == same_size_as_expected) {                    \
-      for(size_t i = 0; i < size_of_formal_bytecode; i++) {                   \
-        assert_that_charptr(res[i] equals to(expected_result)[i]);            \
-      }                                                                       \
-    } else {                                                                  \
-      fail("formal bytecode has different size than expected.");              \
-    }                                                                         \
+#define assert_that_vector(actual, expected)                                \
+  size_t size_of_formal_bytecode = vector_size(actual);                     \
+  size_t same_size_as_expected   = vector_size(expected);                   \
+  assert_that_int(size_of_formal_bytecode equals to same_size_as_expected); \
+  do {                                                                      \
+    if(size_of_formal_bytecode == same_size_as_expected) {                  \
+      for(size_t i = 0; i < size_of_formal_bytecode; i++) {                 \
+        assert_that_charptr((actual)[i] equals to(expected)[i]);            \
+      }                                                                     \
+    } else {                                                                \
+      fail("formal bytecode has different size than expected.");            \
+    }                                                                       \
   } while(0)
 
-#define debug(code, expected_result)                                          \
-  do {                                                                        \
-    VM *vm     = vm_new("file.marg");                                         \
-    vm->source = string_new(code);                                            \
-    lexer_make_tokens(vm);                                                    \
-    parser_analyze_syntax(vm);                                                \
-    char **res                     = vm->formal_bytecode;                     \
-    size_t size_of_formal_bytecode = vector_size(res);                        \
-    size_t same_size_as_expected   = vector_size(expected_result);            \
-    vector_display(res, "%s");                                                \
-                                                                              \
-    assert_that_int(size_of_formal_bytecode equals to same_size_as_expected); \
-                                                                              \
-    if(size_of_formal_bytecode == same_size_as_expected) {                    \
-      for(size_t i = 0; i < size_of_formal_bytecode; i++) {                   \
-        assert_that_charptr(res[i] equals to(expected_result)[i]);            \
-      }                                                                       \
-    } else {                                                                  \
-      fail("formal bytecode has different size than expected.");              \
-    }                                                                         \
+#define parse(code, expected_result)                          \
+  do {                                                        \
+    VM *vm     = vm_new("file.marg");                         \
+    vm->source = string_new(code);                            \
+    lexer_make_tokens(vm);                                    \
+    parser_analyze_syntax(vm);                                \
+    assert_that_vector(vm->formal_bytecode, expected_result); \
+  } while(0)
+
+#define debug(code, expected_result)           \
+  do {                                         \
+    VM *vm     = vm_new("file.marg");          \
+    vm->source = string_new(code);             \
+    lexer_make_tokens(vm);                     \
+    parser_analyze_syntax(vm);                 \
+    vector_display(vm->formal_bytecode, "%s"); \
   } while(0)
 
 #define error(code, error_message, token)                     \
