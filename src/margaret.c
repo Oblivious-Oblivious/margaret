9d5def2b7cee79dc334c54b017d7d4af972e8ef2
oblivious <thanos.papap42@gmail.com>
2024-09-27 03:15:44 +0300

    Proposal for a cc0-v1 license.

--- src/margaret.c
+++ src/margaret.c
@@ -1,23 +1,9 @@
 /**
- * An object oriented language system based on ruby and smalltalk.
- *
- * Copyright (C) 2024 Athanasios Papapostolou (oblivious)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * @author oblivious (https://www.dreamnotexpiring.com)
+ * @brief A developer's only tool (https://www.margaret-lang.org)
+ * @date Copyright (C) 2022-2024 Athanasios Papapostolou (oblivious)
+ * @copyright Margaret by oblivious is marked with CC0 1.0 Universal
  */
-
-/* TODO - Potentially switch to MIT from GPLv3 */
 
 #include "version.h"
 #include "vm/compilation_pipeline.h"
