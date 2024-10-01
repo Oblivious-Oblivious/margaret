6d6bb82ce8b94752ffad2cb3d705b288aab9c959
oblivious <thanos.papap42@gmail.com>
2024-10-01 22:17:40 +0300

    [vm-without-compiled-file] - Removed unnecessary abstractions and reworked dispatch table.

--- z/main.c
+++ z/main.c
@@ -74,17 +74,34 @@ typedef size_t Value;
 #define MAX_REGISTERS (2 << 7)
 #define MAX_CONSTANTS (2 << 7)
 
-#define O(op)             ((op) << 24)
-#define OAk(op, a, k)     (((op) << 24) | ((a) << 16) | ((k) & 0x0000ffff))
-#define OABk(op, a, bk)   (((op) << 24) | ((a) << 16) | ((bk) & 0x0000ffff))
-#define OA(op, a)         (((op) << 24) | ((a) << 16))
-#define OAB(op, a, b)     (((op) << 24) | ((a) << 16) | ((b) << 8))
-#define OABC(op, a, b, c) (((op) << 24) | ((a) << 16) | ((b) << 8) | ((c) << 0))
+#define O(op) vector_add(bc, ((op) << 24))
+#define OAk(op, a, k)                               \
+  vector_add(                                       \
+    bc,                                             \
+    (((op) << 24) | (get_register(vm, (a)) << 16) | \
+     (make_constant(vm, (k)) & 0x0000ffff))         \
+  )
+#define OABk(op, a, bk)                             \
+  vector_add(                                       \
+    bc,                                             \
+    (((op) << 24) | (get_register(vm, (a)) << 16) | \
+     (make_constant(vm, (bk)) & 0x0000ffff))        \
+  )
+#define OA(op, a)     vector_add(bc, (((op) << 24) | (get_register(vm, (a)) << 16)))
+#define OAB(op, a, b) vector_add(bc, (((op) << 24) | ((a) << 16) | ((b) << 8)))
+#define OABC(op, a, b, c)                                         \
+  vector_add(                                                     \
+    bc,                                                           \
+    (((op) << 24) | (get_register(vm, (a)) << 16) |               \
+     (get_register(vm, (b)) << 8) | (get_register(vm, (c)) << 0)) \
+  )
 
 #define FETCH(i) (((i) & 0xff000000) >> 24)
 #define A(i)     (((i) & 0x00ff0000) >> 16)
 #define B(i)     (((i) & 0x0000ff00) >> 8)
 #define C(i)     (((i) & 0x000000ff) >> 0)
+/* TODO - Implement Ak */
+#define Bk(i)    ((B(i) << 8) | C(i))
 
 typedef struct {
   Value registers[MAX_REGISTERS];
@@ -100,6 +117,7 @@ typedef struct Object {
   bool is_marked;
   Type type;
   struct Object *next;
+  VM *bound_vm;
 } Object;
 
 Object *value_object_new(VM *bound_vm, size_t size, Type type) {
@@ -108,6 +126,7 @@ Object *value_object_new(VM *bound_vm, size_t size, Type type) {
   self->is_marked = false;
   self->type      = type;
   self->next      = NULL;
+  self->bound_vm  = bound_vm;
 
   return self;
 }
@@ -191,7 +210,7 @@ uint8_t make_constant(VM *vm, Value value) {
 
 void vm_execute(VM *vm) {
 #if defined(__GNUC__) || defined(__clang__)
-  static void *_computed_gotos[] = {
+  static void *_computed_gotos[256] = {
     &&_computed_goto_OP_NIL,
     &&_computed_goto_OP_FALSE,
     &&_computed_goto_OP_TRUE,
@@ -204,7 +223,250 @@ void vm_execute(VM *vm) {
     &&_computed_goto_OP_JUMP,
     &&_computed_goto_OP_JUMP_IF_FALSE,
     &&_computed_goto_OP_PRINT,
-    &&_computed_goto_OP_HALT
+    &&_computed_goto_OP_HALT,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR,
+    &&_computed_goto_ERROR
   };
   #define switch_opcode       \
     uint32_t i;               \
@@ -213,7 +475,7 @@ void vm_execute(VM *vm) {
     goto *_computed_gotos[FETCH(i)];
   #define case_opcode(op) _computed_goto_##op:
   #define default_opcode \
-  _err:
+  _computed_goto_ERROR:
 #else
   #define switch_opcode       \
     uint32_t i;               \
@@ -231,112 +493,93 @@ void vm_execute(VM *vm) {
 
   switch_opcode {
     case_opcode(OP_NIL) {
-      uint8_t a        = A(i);
-      vm->registers[a] = NIL();
+      vm->registers[A(i)] = NIL();
       next_opcode;
     }
     case_opcode(OP_FALSE) {
-      uint8_t a        = A(i);
-      vm->registers[a] = FALSE();
+      vm->registers[A(i)] = FALSE();
       next_opcode;
     }
     case_opcode(OP_TRUE) {
-      uint8_t a        = A(i);
-      vm->registers[a] = TRUE();
+      vm->registers[A(i)] = TRUE();
       next_opcode;
     }
     case_opcode(OP_NUMBER) {
-      uint8_t a        = A(i);
-      uint16_t k       = (B(i) << 8) | C(i);
-      vm->registers[a] = vm->constants[k];
+      vm->registers[A(i)] = vm->constants[Bk(i)];
       next_opcode;
     }
     case_opcode(OP_STRING) {
-      uint8_t a        = A(i);
-      uint16_t k       = (B(i) << 8) | C(i);
-      vm->registers[a] = vm->constants[k];
+      vm->registers[A(i)] = vm->constants[Bk(i)];
       next_opcode;
     }
     case_opcode(OP_ADD) {
-      uint8_t a        = A(i);
-      uint8_t b        = B(i);
-      uint8_t c        = C(i);
-      vm->registers[a] = NUMBER(
-        AS_NUMBER(vm->registers[b])->value + AS_NUMBER(vm->registers[c])->value
+      vm->registers[A(i)] = NUMBER(
+        AS_NUMBER(vm->registers[B(i)])->value +
+        AS_NUMBER(vm->registers[C(i)])->value
       );
       next_opcode;
     }
     case_opcode(OP_SUB) {
-      uint8_t a        = A(i);
-      uint8_t b        = B(i);
-      uint8_t c        = C(i);
-      vm->registers[a] = NUMBER(
-        AS_NUMBER(vm->registers[b])->value - AS_NUMBER(vm->registers[c])->value
+      vm->registers[A(i)] = NUMBER(
+        AS_NUMBER(vm->registers[B(i)])->value -
+        AS_NUMBER(vm->registers[C(i)])->value
       );
       next_opcode;
     }
     case_opcode(OP_MUL) {
-      uint8_t a        = A(i);
-      uint8_t b        = B(i);
-      uint8_t c        = C(i);
-      vm->registers[a] = NUMBER(
-        AS_NUMBER(vm->registers[b])->value * AS_NUMBER(vm->registers[c])->value
+      vm->registers[A(i)] = NUMBER(
+        AS_NUMBER(vm->registers[B(i)])->value *
+        AS_NUMBER(vm->registers[C(i)])->value
       );
       next_opcode;
     }
     case_opcode(OP_DIV) {
-      uint8_t a      = A(i);
-      uint8_t b      = B(i);
-      uint8_t c      = C(i);
-      double divisor = AS_NUMBER(vm->registers[c])->value;
-      if(divisor == 0.0) {
+      if(AS_NUMBER(vm->registers[C(i)])->value == 0.0) {
         fprintf(stderr, "Runtime Error: Division by zero\n");
         exit(1);
       }
-      vm->registers[a] = NUMBER(AS_NUMBER(vm->registers[b])->value / divisor);
+      vm->registers[A(i)] = NUMBER(
+        AS_NUMBER(vm->registers[B(i)])->value /
+        AS_NUMBER(vm->registers[C(i)])->value
+      );
       next_opcode;
     }
     case_opcode(OP_JUMP) {
-      uint8_t a  = A(i);
-      uint16_t k = (B(i) << 8) | C(i);
-      if(k >= MAX_CONSTANTS) {
-        fprintf(stderr, "Invalid jump target: %u\n", k);
+      if(Bk(i) >= MAX_CONSTANTS) {
+        fprintf(stderr, "Invalid jump target: %u\n", Bk(i));
         exit(1);
       }
-      vm->ip = k;
+      vm->ip = Bk(i);
       skip_opcode;
     }
     case_opcode(OP_JUMP_IF_FALSE) {
-      uint8_t a       = A(i);
-      uint16_t k      = (B(i) << 8) | C(i);
-      Value condition = vm->registers[a];
+      Value condition = vm->registers[A(i)];
       bool is_false   = IS_FALSE(condition) || IS_NIL(condition);
       if(is_false) {
-        if(k >= MAX_CONSTANTS) {
-          fprintf(stderr, "Invalid jump target: %u\n", k);
+        if(Bk(i) >= MAX_CONSTANTS) {
+          fprintf(stderr, "Invalid jump target: %u\n", Bk(i));
           exit(1);
         }
-        vm->ip = k;
+        vm->ip = Bk(i);
         skip_opcode;
       } else {
         next_opcode;
       }
     }
     case_opcode(OP_PRINT) {
-      uint8_t a = A(i);
-      Value v   = vm->registers[a];
+      Value v = vm->registers[A(i)];
       if(IS_NIL(v)) {
-        printf("R%d = nil\n", a);
+        printf("R%d = nil\n", A(i));
       } else if(IS_FALSE(v)) {
-        printf("R%d = false\n", a);
+        printf("R%d = false\n", A(i));
       } else if(IS_TRUE(v)) {
-        printf("R%d = true\n", a);
+        printf("R%d = true\n", A(i));
       } else if(IS_NUMBER(v)) {
-        printf("R%d = %g\n", a, AS_NUMBER(v)->value);
+        printf("R%d = %g\n", A(i), AS_NUMBER(v)->value);
       } else if(IS_STRING(v)) {
-        printf("R%d = \"%s\"\n", a, AS_STRING(v)->value);
+        printf("R%d = \"%s\"\n", A(i), AS_STRING(v)->value);
       } else {
-        printf("R%d = UNKNOWN\n", a);
+        printf("R%d = UNKNOWN\n", A(i));
       }
       next_opcode;
     }
@@ -363,51 +606,37 @@ uint8_t get_register(VM *vm, const char *var) {
   }
 }
 
-#define emit(i)                  vector_add(bc, i)
-#define emit_O(opcode)           emit(O(opcode))
-#define emit_OAk(opcode, var, k) emit(OAk(opcode, get_register(vm, var), k))
-#define emit_OA(opcode, var_a)   emit(OA(opcode, get_register(vm, var_a)))
-#define emit_OABk(opcode, var_a, var_bk) \
-  emit(OABk(opcode, get_register(vm, var_a), var_bk))
-#define emit_OABC(opcode, var_a, var_b, var_c) \
-  emit(OABC(                                   \
-    opcode,                                    \
-    get_register(vm, var_a),                   \
-    get_register(vm, var_b),                   \
-    get_register(vm, var_c)                    \
-  ))
-
 uint32_t *emit_example_bytecode(VM *vm) {
   uint32_t *bc = NULL;
 
-  emit_OAk(OP_NIL, "x", make_constant(vm, NIL()));
-  emit_OAk(OP_FALSE, "y", make_constant(vm, FALSE()));
-  emit_OAk(OP_TRUE, "y2", make_constant(vm, TRUE()));
-  emit_OAk(OP_NUMBER, "y3", make_constant(vm, NUMBER(1)));
-  emit_OAk(OP_NUMBER, "z", make_constant(vm, NUMBER(10)));
-  emit_OAk(OP_NUMBER, "a", make_constant(vm, NUMBER(3.14)));
-  emit_OAk(OP_STRING, "msg", make_constant(vm, STRING("Hello")));
-  emit_OABC(OP_ADD, "result_add", "z", "a");
-  emit_OABC(OP_SUB, "result_sub", "result_add", "y3");
-  emit_OABC(OP_MUL, "result_mul", "result_sub", "z");
-  emit_OABC(OP_DIV, "result_div", "result_mul", "a");
-  emit_OA(OP_PRINT, "x");
-  emit_OA(OP_PRINT, "y");
-  emit_OA(OP_PRINT, "y2");
-  emit_OA(OP_PRINT, "y3");
-  emit_OA(OP_PRINT, "z");
-  emit_OA(OP_PRINT, "a");
-  emit_OA(OP_PRINT, "msg");
-  emit_OA(OP_PRINT, "result_add");
-  emit_OA(OP_PRINT, "result_sub");
-  emit_OA(OP_PRINT, "result_mul");
-  emit_OA(OP_PRINT, "result_div");
-  emit_O(OP_HALT);
+  OAk(OP_NIL, "x", NIL());
+  OAk(OP_FALSE, "y", FALSE());
+  OAk(OP_TRUE, "y2", TRUE());
+  OAk(OP_NUMBER, "y3", NUMBER(1));
+  OAk(OP_NUMBER, "z", NUMBER(10));
+  OAk(OP_NUMBER, "a", NUMBER(3.14));
+  OAk(OP_STRING, "msg", STRING("Hello"));
+  OABC(OP_ADD, "result_add", "z", "a");
+  OABC(OP_SUB, "result_sub", "result_add", "y3");
+  OABC(OP_MUL, "result_mul", "result_sub", "z");
+  OABC(OP_DIV, "result_div", "result_mul", "a");
+  OA(OP_PRINT, "x");
+  OA(OP_PRINT, "y");
+  OA(OP_PRINT, "y2");
+  OA(OP_PRINT, "y3");
+  OA(OP_PRINT, "z");
+  OA(OP_PRINT, "a");
+  OA(OP_PRINT, "msg");
+  OA(OP_PRINT, "result_add");
+  OA(OP_PRINT, "result_sub");
+  OA(OP_PRINT, "result_mul");
+  OA(OP_PRINT, "result_div");
+  O(OP_HALT);
 
   return bc;
 }
 
-int main(int argc, char **argv) {
+int main(void) {
   VM _vm = {0};
   VM *vm = &_vm;
 
