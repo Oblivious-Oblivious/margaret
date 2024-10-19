c6e8d76ba484051aca6016684a2069ac965988b3
oblivious <thanos.papap42@gmail.com>
2024-10-19 18:26:31 +0300

    [master] - Added rest of the specs.

--- spec/margaret.spec.c
+++ spec/margaret.spec.c
@@ -1,51 +1,51 @@
-// /* margaret */
-// #include "version.spec.h"
+/* margaret */
+#include "version.spec.h"
 
-// /* evaluator */
-// #include "evaluator/Evaluator.spec.h"
+/* evaluator */
+#include "evaluator/Evaluator.spec.h"
 
-// /* generic */
-// #include "generic/arithmetic.spec.h"
-// #include "generic/assignments.spec.h"
-// #include "generic/bitstrings.spec.h"
-// #include "generic/bitwise.spec.h"
-// #include "generic/booleans.spec.h"
-// #include "generic/characters.spec.h"
-// #include "generic/complex.spec.h"
-// #include "generic/conditionals.spec.h"
-// #include "generic/constants.spec.h"
-// #include "generic/conversions.spec.h"
-// #include "generic/date_time.spec.h"
-// #include "generic/errors.spec.h"
-// #include "generic/groups.spec.h"
-// #include "generic/hashes.spec.h"
-// #include "generic/headless_methods.spec.h"
-// #include "generic/identifiers.spec.h"
-// #include "generic/intervals.spec.h"
-// #include "generic/iterations.spec.h"
-// #include "generic/margaret_messages.spec.h"
-// #include "generic/messages.spec.h"
-// #include "generic/meta_evaluation.spec.h"
-// #include "generic/method_definition.spec.h"
-// #include "generic/numeric.spec.h"
-// #include "generic/postcard.spec.h"
-// #include "generic/rational.spec.h"
-// #include "generic/strings.spec.h"
-// #include "generic/tensors.spec.h"
-// #include "generic/tuples.spec.h"
-// #include "generic/unicode.spec.h"
-// #include "generic/variables.spec.h"
+/* generic */
+#include "generic/arithmetic.spec.h"
+#include "generic/assignments.spec.h"
+#include "generic/bitstrings.spec.h"
+#include "generic/bitwise.spec.h"
+#include "generic/booleans.spec.h"
+#include "generic/characters.spec.h"
+#include "generic/complex.spec.h"
+#include "generic/conditionals.spec.h"
+#include "generic/constants.spec.h"
+#include "generic/conversions.spec.h"
+#include "generic/date_time.spec.h"
+#include "generic/errors.spec.h"
+#include "generic/groups.spec.h"
+#include "generic/hashes.spec.h"
+#include "generic/headless_methods.spec.h"
+#include "generic/identifiers.spec.h"
+#include "generic/intervals.spec.h"
+#include "generic/iterations.spec.h"
+#include "generic/margaret_messages.spec.h"
+#include "generic/messages.spec.h"
+#include "generic/meta_evaluation.spec.h"
+#include "generic/method_definition.spec.h"
+#include "generic/numeric.spec.h"
+#include "generic/postcard.spec.h"
+#include "generic/rational.spec.h"
+#include "generic/strings.spec.h"
+#include "generic/tensors.spec.h"
+#include "generic/tuples.spec.h"
+#include "generic/unicode.spec.h"
+#include "generic/variables.spec.h"
 
-// /* inspector */
-// #include "inspector/Inspector.spec.h"
+/* inspector */
+#include "inspector/Inspector.spec.h"
 
-// /* lexer */
-// #include "lexer/alternate_to_dec.spec.h"
-// #include "lexer/Lexer.spec.h"
-// #include "lexer/token_types.spec.h"
+/* lexer */
+#include "lexer/alternate_to_dec.spec.h"
+#include "lexer/Lexer.spec.h"
+#include "lexer/token_types.spec.h"
 
-// /* loader */
-// #include "loader/Loader.spec.h"
+/* loader */
+#include "loader/Loader.spec.h"
 
 /* opcode */
 #include "opcode/MargBitstring.spec.h"
@@ -62,71 +62,70 @@
 #include "opcode/MargTuple.spec.h"
 #include "opcode/MargValue.spec.h"
 
-// /* optimizer */
-// #include "optimizer/Optimizer.spec.h"
+/* optimizer */
+#include "optimizer/Optimizer.spec.h"
 
-// /* parser */
-// #include "parser/Parser.spec.h"
+/* parser */
+#include "parser/Parser.spec.h"
 
-// /* scanner */
-// #include "scanner/Scanner.spec.h"
+/* scanner */
+#include "scanner/Scanner.spec.h"
 
-// /* tokens */
-// #include "tokens/Token.spec.h"
-// #include "tokens/Type.spec.h"
+/* tokens */
+#include "tokens/Token.spec.h"
+#include "tokens/Type.spec.h"
 
-// /* vm */
-// #include "vm/byte_conversions.spec.h"
-// #include "vm/vm.spec.h"
+/* vm */
+#include "vm/byte_conversions.spec.h"
+#include "vm/vm.spec.h"
 
 int main(void) {
   cspec_run_suite("all", {
-    // VersionSpec();
+    VersionSpec();
 
-    // EvaluatorSpec();
+    EvaluatorSpec();
 
-    // arithmetic_spec();
-    // assignments_spec();
-    // bitstrings_spec();
-    // bitwise_spec();
-    // booleans_spec();
-    // characters_spec();
-    // complex_spec();
-    // conditionals_spec();
-    // constants_spec();
-    // conversions_spec();
-    // date_time_spec();
-    // errors_spec();
-    // groups_spec();
-    // hashes_spec();
-    // headless_methods_spec();
-    // identifiers_spec();
-    // intervals_spec();
-    // iterations_spec();
-    // margaret_messages_spec();
-    // messages_spec();
-    // meta_evaluation_spec();
-    // method_definition_spec();
-    // numeric_spec();
-    // postcard_spec();
-    // rational_spec();
-    // strings_spec();
-    // tensors_spec();
-    // tuples_spec();
-    // unicode_spec();
-    // variables_spec();
+    arithmetic_spec();
+    assignments_spec();
+    bitstrings_spec();
+    bitwise_spec();
+    booleans_spec();
+    characters_spec();
+    complex_spec();
+    conditionals_spec();
+    constants_spec();
+    conversions_spec();
+    date_time_spec();
+    errors_spec();
+    groups_spec();
+    hashes_spec();
+    headless_methods_spec();
+    identifiers_spec();
+    intervals_spec();
+    iterations_spec();
+    margaret_messages_spec();
+    messages_spec();
+    meta_evaluation_spec();
+    method_definition_spec();
+    numeric_spec();
+    postcard_spec();
+    rational_spec();
+    strings_spec();
+    tensors_spec();
+    tuples_spec();
+    unicode_spec();
+    variables_spec();
 
-    // InspectorSpec();
+    InspectorSpec();
 
-    // alternate_to_dec_spec();
-    // LexerSpec();
-    // token_types_spec();
+    alternate_to_dec_spec();
+    LexerSpec();
+    token_types_spec();
 
-    // LoaderSpec();
+    LoaderSpec();
 
     MargBitstringSpec();
     MargFloatSpec();
-    MargHashSpec();
     MargIntegerSpec();
     MargLabelSpec();
     MargMethodSpec();
@@ -134,20 +133,21 @@ int main(void) {
     MargSingletonsSpec();
     MargStringSpec();
     MargSymbolSpec();
+    MargHashSpec();
     MargTensorSpec();
     MargTupleSpec();
     MargValueSpec();
 
-    // OptimizerSpec();
+    OptimizerSpec();
 
-    // ParserSpec();
+    ParserSpec();
 
-    // ScannerSpec();
+    ScannerSpec();
 
-    // TokenSpec();
-    // TypeSpec();
+    TokenSpec();
+    TypeSpec();
 
-    // byte_conversions_spec();
-    // VmSpec();
+    byte_conversions_spec();
+    VmSpec();
   });
 }
