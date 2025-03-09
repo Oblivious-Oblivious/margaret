90dbfe34d6eabb2dd5db8bd8c675f1ffa5bb999b
oblivious <atha@fourth-wall.org>
2025-03-10 01:33:05 +0200

    [master] - Refactored readline to use the system dependency instead of the emeralds library.
    Using lua's technique in handling this by using fputs in case of non-readline system.  User cross compiles with explicit flag.

--- src/scanner/Scanner.c
+++ src/scanner/Scanner.c
@@ -1,20 +1,36 @@
 #include "Scanner.h"
 
-#include "../../libs/EmeraldsReadline/export/EmeraldsReadline.h"
 #include "../../libs/EmeraldsString/export/EmeraldsString.h"
 
+#if defined(MARG_USE_READLINE)
+  #include <readline/history.h>  /* add_history, get_history */
+  #include <readline/readline.h> /* readline */
+#else
+  #include <stdio.h> /* fgets, fputs, fflush */
+#endif
+
 VM *scanner_scan(VM *vm, char *prompt) {
-  char *line = readline(prompt);
+  char line_buf[512];
+  char *line = line_buf;
+#if defined(MARG_USE_READLINE)
+  line = readline(prompt);
+#else
+  fputs(prompt, stdout);
+  fflush(stdout);
+  fgets(line, 512, stdin);
+#endif
 
   if(line == NULL) {
     exit(0);
   } else if(*line == '\0') {
     vm->source = string_new("");
   } else {
+#if defined(MARG_USE_READLINE)
     HIST_ENTRY *prev = history_length > 0 ? history_get(history_length) : NULL;
     if(!prev || !string_equals(string_new(line), string_new(prev->line))) {
       add_history(line);
     }
+#endif
     vm->source = string_new(line);
   }
 
